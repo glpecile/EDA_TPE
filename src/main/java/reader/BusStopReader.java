2cd16fae77e5d38975e4356043982445fbc19fd7
rcatalan98 <robertitocatalan@gmail.com>
2020-09-30 11:06:03 -0300

    Se agrego lo basico de la lectura de CSV.

--- src/main/java/reader/BusStopReader.java
+++ src/main/java/reader/BusStopReader.java
@@ -1,4 +1,17 @@
 package reader;
 
+
+import java.io.IOException;
+import java.util.List;
+
 public class BusStopReader {
+    private final String filePath = "/resources/espacios-culturales.csv";
+    private final List<String[]> busStopData;
+    public BusStopReader() throws IOException {
+        ReadResources  resources = new ReadResources(filePath);
+        busStopData = resources.gettingFileReady();
+    }
+
+
+
 }
---
+++ src/main/java/reader/ReadResources.java
@@ -0,0 +1,27 @@
+package reader;
+
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+public class ReadResources {
+    private final String filePath;
+    public ReadResources(String filePath) {
+        this.filePath = filePath;
+
+    }
+
+    public List<String[]> gettingFileReady() throws IOException {
+        FileReader filereader = new FileReader(filePath);
+        CSVParser parser = new CSVParserBuilder().withSeparator(';').build();
+        CSVReader csvReader = new CSVReaderBuilder(filereader).withSkipLines(1).withCSVParser(parser).build();
+        List<String[]> allData = csvReader.readAll();
+        return allData;
+    }
+
+}
