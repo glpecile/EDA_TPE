646ec0ad3b2897af3dfa1bcba9ccade8957b849e
rcatalan98 <robertitocatalan@gmail.com>
2020-09-30 11:28:01 -0300

    Se agrego la clase BusStop.java y el archivo recorrido-colectivos.csv

--- src/main/java/reader/BusStopReader.java
+++ src/main/java/reader/BusStopReader.java
@@ -1,17 +1,23 @@
 package reader;
 
 
+import model.BusStop;
+
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class BusStopReader {
     private final String filePath = "/resources/espacios-culturales.csv";
     private final List<String[]> busStopData;
+    private Set<BusStop> busStops = new HashSet<>();
     public BusStopReader() throws IOException {
         ReadResources  resources = new ReadResources(filePath);
         busStopData = resources.gettingFileReady();
     }
 
-
-
+    public Set<BusStop> getBusStops() {
+        return busStops;
+    }
 }
