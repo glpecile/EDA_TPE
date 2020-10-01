45586a5f2cbb0e598094f3e44ab3cbcb48aa188f
juarce <juarce@itba.edu.ar>
2020-10-01 20:06:02 -0300

    Graph class

---
+++ src/main/java/pathfinder/Graph.java
@@ -0,0 +1,18 @@
+package pathfinder;
+
+public class Graph {
+
+
+    public static class Node {
+        private double lat;
+        private double lng;
+        private String line;
+        private int line_id;
+
+
+    }
+
+    public static class Edge {
+
+    }
+}
