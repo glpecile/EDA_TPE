e9ba45756cb4580c9a44bb0088048b05a1e51db1
Roberto Catalan <rcatalan@itba.edu.ar>
2020-10-01 22:12:40 -0300

    Se agrega Coord como clase general.

---
+++ src/main/java/model/Coord.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Coord {
+    private final double lat;
+    private final double lng;
+
+    public Coord(double lat, double lng) {
+        this.lat = lat;
+        this.lng = lng;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public double getLng() {
+        return lng;
+    }
+}
